d55475a0b9bdea61a1779ee568e75626a026a65f
computeristgeek <salahuddin.kazak@gmail.com>
2016-01-06 15:55:17 +0200

    Update Underconstruction.php with navbar

--- underconstruction.php
+++ underconstruction.php
@@ -1,12 +1,12 @@
 <?php include 'header.php' ?>
-  <body >
+  <body style="padding-top: 50px;">
+<?php include('navbar.php'); ?>
     <div class="container">
 	    <div class="row">
-		<div class="col-xs-12 navmenuitem" id="feestitle">
-		    <p>Under Construction</p>
+		<div class="page-header">
+		  <h1>Under Construction</h1>
 		</div>
 	    </div>
-    <hr>
 	    <img class="center-block" src="img/under-construction-150271_1280.png" />
     </div>
 <?php include 'footer.php' ?>
