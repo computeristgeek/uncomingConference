d6fbc6638709fdb779f3a633c595b507ae5131bb
computeristgeek <salahuddin.kazak@gmail.com>
2016-01-06 16:38:20 +0200

    Fix Sponsors Typos

--- sponsors.php
+++ sponsors.php
@@ -12,7 +12,7 @@
 		<div class="col-md-12">
 			  <h2 class="featurette-heading">Package Options</h2>
 			  <table class="table">
-				<tbody><tr><th>Package Options</th><th>Main Items Included</th><th>Advertisement in the Brochure</th><th>Amount in EGP (unit price)</th></tr>
+				<tbody><tr><th>Package Options</th><th>Main Items Included</th><th>Advertisement in the Program Pamphlet</th><th>Amount in EGP (unit price)</th></tr>
 				<tr>
 					<td>Diamond Sponsor</td>
 					<td>
@@ -21,10 +21,10 @@
 						<li>One sponsorship banner</li>
 						<li>One exhibition booth in PolyU venue during conference period</li>
 						<li>One free industrial presentation</li>
-						<li>4 free registrations.</li>
+						<li>4 free registrations</li>
 						</ul>
 					</td>
-					<td>Additional 4 full inside page colour advertisement</td>
+					<td>Additional 4 full inside page colour advertisements</td>
 					<td>30,000</td>
 				</tr>
 				<tr>
@@ -34,10 +34,10 @@
 						<li>One sponsorship banner</li>
 						<li>One exhibition booth in PolyU venue during conference period</li>
 						<li>One free industrial presentation</li>
-						<li>2 free registrations.</li>
+						<li>2 free registrations</li>
 						</ul>
 					</td>
-					<td>Additional 3 full inside page colour advertisement</td>
+					<td>Additional 3 full inside page colour advertisements</td>
 					<td>22,000</td>
 				</tr>
 				<tr>
@@ -46,10 +46,10 @@
 						<ul>
 						<li>One sponsorship banner</li>
 						<li>One free industrial presentation</li>
-						<li>1 free registrations.</li>
+						<li>1 free registration</li>
 						</ul>
 					</td>
-					<td>Additional 2 full inside page colour advertisement</td>
+					<td>Additional 2 full inside page colour advertisements</td>
 					<td>17,000</td>
 				</tr>
 				<tr>
@@ -73,6 +73,6 @@
 		<li>Sponsors are given the opportunity to present their product and services at a formal conference industry session on day 1 of the conference program.  A 30-minute time slot can be arranged on a first-come-first-served basis.   Total 12 sessions available.  Academic delegates and industry professionals will sit in the presentation.</li>
 		<li>All sponsors are welcome to supply us with any promotional materials or premiums that they wish to be attached with the Conference Proceedings package. This is at no additional charges but items much reach us no later than 15 May, 2016.</li>
 		</ol>
-		<br />Please consider sponsoring our event. If you like to do so or would like to make further inquiries please contact us.  All cheques should be made payable to “Pharos University in Alexandria”  At back of cheque, please mark clearly your <company name> and the <sponsorship package number>.  Please mail to Conference Secretariat.</h4>
+		<br />Please consider sponsoring our event. If you like to do so or would like to make further inquiries please contact us.  All cheques should be made payable to “Pharos University in Alexandria”  At back of cheque, please mark clearly your &lt;company name&gt; and &lt;the sponsorship package number&gt;.  Please mail to Conference Secretariat.</h4>
     </div>
 <?php include 'footer.php' ?>
