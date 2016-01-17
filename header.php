b488c8d8a014c9c015e349527e78d45bd105354e
computeristgeek <salahuddin.kazak@gmail.com>
2016-01-17 11:02:07 +0200

    Add Google Analytics

--- header.php
+++ header.php
@@ -26,4 +26,5 @@
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="js/bootstrap.min.js"></script>
     <script src="js/conf.js"></script>
+	<?php include_once("analyticstracking.php") ?>
   </head>
