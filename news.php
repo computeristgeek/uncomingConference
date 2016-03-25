806de04032f201c3c90b3c8d750133ee8ea7e96b
computeristgeek <salahuddin.kazak@gmail.com>
2016-03-25 03:06:11 +0200

    Add News Page (for the rest of the news)

--- news.php
+++ news.php
@@ -1,48 +1,31 @@
 <?php
-	$title='International Conference on New Trends for Sustainable Energy';
-	$keywords='icntse,pua,kth,international conference,new trends,sustainable energy,science,extended abstract,call for papers';
-	$description='The official website for the International Conference on New Trends for Sustainable Energy (ICNTSE) Conference, OCT 1-3, with the cooperation of PUA, Egypt and KTH, Sweden';
+	$title='ICNTSE - NEWS';
+	$keywords='icntse,pua,kth,international conference,new trends,sustainable energy,science,news';
+	$description='ICNTSE RSS FEED NEWS';
 ?>
 <?php include 'header.php' ?>
   <body style="padding-top: 50px;">
 <?php include('navbar.php');?>
     <div class="container">
         <div class="row">
-        	<div class="col-md-4">
-        		<br>
+        	<div class="col-md-12">
+		<div>
         		<div class="panel panel-default">
-						<div class="panel-heading"> <span class="glyphicon glyphicon-list-alt"></span><b>Conference News</b></div>
+						<div class="panel-heading"> <b>ICNTSE News Feed</b></div>
 							<div class="panel-body">
 								<div class="row">
 									<div class="col-xs-12">
-										<ul class="news_list">
-											<li class="news-item">Imprtant dates has been updated, please check the dates again by<a href="home.php#callPapers">Click here</a></li>
-											<li class="news-item">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in venenatis enim... <a href="#">Read more...</a></li>
-											<li class="news-item">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in venenatis enim... <a href="#">Read more...</a></li>
-											<li class="news-item">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in venenatis enim... <a href="#">Read more...</a></li>
-											<li class="news-item">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in venenatis enim... <a href="#">Read more...</a></li>
-											<li class="news-item">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in venenatis enim... <a href="#">Read more...</a></li>
-										</ul>
+	<ul>
+<div> <!-- start feedwind code --><script type="text/javascript">document.write('\x3Cscript type="text/javascript" src="' + ('https:' == document.location.protocol ? 'https://' : 'http://') + 'feed.mikle.com/js/rssmikle.js">\x3C/script>');</script><script type="text/javascript">(function() {var params = {rssmikle_url: "http://www.icntse.com/rss.xml",rssmikle_frame_width: "100",rssmikle_frame_height: "400",frame_height_by_article: "0",rssmikle_target: "_blank",rssmikle_font: "Arial, Helvetica, sans-serif",rssmikle_font_size: "12",rssmikle_border: "off",responsive: "on",rssmikle_css_url: "",text_align: "left",text_align2: "left",corner: "on",scrollbar: "on",autoscroll: "on",scrolldirection: "down",scrollstep: "10",mcspeed: "20",sort: "Off",rssmikle_title: "off",rssmikle_title_sentence: "",rssmikle_title_link: "",rssmikle_title_bgcolor: "#FFFFFF",rssmikle_title_color: "#000000",rssmikle_title_bgimage: "",rssmikle_item_bgcolor: "#FFFFFF",rssmikle_item_bgimage: "",rssmikle_item_title_length: "75",rssmikle_item_title_color: "#000000",rssmikle_item_border_bottom: "on",rssmikle_item_description: "on",item_link: "off",rssmikle_item_description_length: "450",rssmikle_item_description_color: "#000000",rssmikle_item_date: "gl1",rssmikle_timezone: "Etc/GMT",datetime_format: "%b %e, %Y %l:%M %p",item_description_style: "text+tn",item_thumbnail: "full",item_thumbnail_selection: "auto",article_num: "15",rssmikle_item_podcast: "off",keyword_inc: "",keyword_exc: ""};feedwind_show_widget_iframe(params);})();</script><!-- end feedwind code --><!--  end  feedwind code --></div>
+	</ul>
 									</div>
 								</div>
 							</div>
-						<div class="panel-footer"> </div>
+						<div class="panel-footer"><a href="rss.xml"><img id="rssimg" src="img/Rss-feed.svg" alt="Rss Feed"/></a></div>
 				</div>
+			</div>
         	</div>
     	</div>
     </div>
-    <script type="text/javascript">
-    $(function () {
-		$(".news_list").bootstrapNews({
-            newsPerPage: 3,
-            autoplay: true,
-			pauseOnHover:true,
-            direction: 'up',
-            newsTickerInterval: 4000,
-            onToDo: function () {
-                //console.log(this);
-            }
-        });
-    });
-</script>
+	<script>$('#newsfeed').toggle()</script>
 <?php include 'footer.php' ?>
