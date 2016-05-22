12d23e0709fc37d3399096055a2c5ae7881e39e3
Karim Soliman <karim.solimman@gmail.com>
2016-05-22 14:27:54 +0200

    Keynote - Speakers file updated

--- keynote.php
+++ keynote.php
@@ -35,30 +35,30 @@
 	    <div class="row">
 		  <div class="col-sm-6 col-md-4">
 		    <div class="thumbnail">
-		      <img src="img/avatar.png" alt="Speaker Avatar">
+		      <img src="img/speaker/1.jpg" class="img-circle" alt="Speaker1">
 		      <div class="caption">
-		        <h3><center>Speaker one</center></h3>
-		        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+		        <h3><center>Professor/vice president  Ramon Wyss, KTH</center></h3>
+		        <p>Challenge driven education for energy system transformations</p>
 		      </div>
 		    </div>
 		  </div>
 
 		  <div class="col-sm-6 col-md-4">
 		    <div class="thumbnail">
-		      <img src="img/avatar.png" alt="Speaker Avatar">
+		      <img src="img/speaker/2.jpg" class="img-circle" alt="Speaker2">
 		      <div class="caption">
-		        <h3><center>Speaker Two</center></h3>
-		        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+		        <h3><center>Professor Mark Howells, KTH</center></h3>
+		        <p>African Energy Renaissance – An immense investment, technology and sustainable development prospect</p>
 		      </div>
 		    </div>
 		  </div>
 
 		  <div class="col-sm-6 col-md-4">
 		    <div class="thumbnail">
-		      <img src="img/avatar.png" alt="Speaker Avatar">
+		      <img src="img/speaker/3.jpg" class="img-circle" alt="Speaker3">
 		      <div class="caption">
-		        <h3><center>Speaker Three</center></h3>
-		        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+		        <h3><center>Professor SemidaSilveira, KTH</center></h3>
+		        <p>(TBD) - Traffic solutions or waste recycling</p>
 		      </div>
 		    </div>
 		  </div>
@@ -67,33 +67,97 @@
 		<div class="row">
 		  <div class="col-sm-6 col-md-4">
 		    <div class="thumbnail">
-		      <img src="img/avatar.png" alt="Speaker Avatar">
+		      <img src="img/speaker/4.jpg" class="img-circle" alt="Speaker4">
 		      <div class="caption">
-		        <h3><center>Speaker Four</center></h3>
-		        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+		        <h3><center>Professor Mamoun Muhammed, KTH</center></h3>
+		        <p>TBD</p>
 		      </div>
 		    </div>
 		  </div>
 
 		<div class="col-sm-6 col-md-4">
 		    <div class="thumbnail">
-		      <img src="img/avatar.png" alt="Speaker Avatar">
+		      <img src="img/speaker/5.jpg" class="img-circle" alt="Speaker5">
 		      <div class="caption">
-		        <h3><center>Speaker Five</center></h3>
-		        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+		        <h3><center>Professor Klas Engwall, KTH</center></h3>
+		        <p>Trends in thermochemical conversion of waste and biomass</p>
 		      </div>
 		    </div>
 		  </div>
 
 		  <div class="col-sm-6 col-md-4">
 		    <div class="thumbnail">
-		      <img src="img/avatar.png" alt="Speaker Avatar">
+		      <img src="img/avatar.png" class="img-circle" alt="Speaker6">
 		      <div class="caption">
-		        <h3><center>Speaker Six</center></h3>
-		        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+		        <h3><center>Professor Åsa Stenmark</center></h3>
+		        <p>Waste streams for chemical conversion technologies (provisional)</p>
 		      </div>
 		    </div>
 		  </div>
+		</div>
+
+		<div class="row">
+		  <div class="col-sm-6 col-md-4">
+		    <div class="thumbnail">
+		      <img src="img/speaker/7.jpg" class="img-circle" alt="Speaker7">
+		      <div class="caption">
+		        <h3><center>Professor Björn Palm, KTH</center></h3>
+		        <p>Solar Cooling, status and perspectives</p>
+		      </div>
+		    </div>
+		  </div>
+
+		  <div class="col-sm-6 col-md-4">
+		    <div class="thumbnail">
+		      <img src="img/speaker/8.jpg" class="img-circle" alt="Speaker8">
+		      <div class="caption">
+		        <h3><center>Professor Göran Engdahl, KTH</center></h3>
+		        <p>TBD</p>
+		      </div>
+		    </div>
+		  </div>
+
+		  <div class="col-sm-6 col-md-4">
+		    <div class="thumbnail">
+		      <img src="img/speaker/9.jpg" class="img-circle" alt="Speaker9">
+		      <div class="caption">
+		        <h3><center>Professor Alexis Pontvik, KTH</center></h3>
+		        <p>TBD.</p>
+		      </div>
+		    </div>
+		  </div>
+		</div>
+		<div class="row">
+		  <div class="col-sm-6 col-md-4">
+		    <div class="thumbnail">
+		      <img src="img/speaker/10.jpg" class="img-circle" alt="Speaker10">
+		      <div class="caption">
+		        <h3><center>Professor Yohannes Kiros, KTH</center></h3>
+		        <p>Alternative catalysts for anodic and cathodic reactions in water electrolysis</p>
+		      </div>
+		    </div>
+		  </div>
+
+		  <div class="col-sm-6 col-md-4">
+		    <div class="thumbnail">
+		      <img src="img/speaker/11.jpg" class="img-circle" alt="Speaker11">
+		      <div class="caption">
+		        <h3><center>Professor Björn Laumert, KTH</center></h3>
+		        <p>The role and future potential of CSP in the MENA region.</p>
+		      </div>
+		    </div>
+		  </div>
+
+		  <div class="col-sm-6 col-md-4">
+		    <div class="thumbnail">
+		      <img src="img/speaker/12.jpg" class="img-circle" alt="Speaker12">
+		      <div class="caption">
+		        <h3><center>Professor Cristian Bogdan, KTH</center></h3>
+		        <p>TBD</p>
+		      </div>
+		    </div>
+		  </div>
+		</div>  
 		</div>
 	</div>
 <script type="text/javascript" src="js/bootstrap.js"></script>
