e8893d293beb41dfb12fcd77114c1e941d7ff017
Karim Soliman <karim.solimman@gmail.com>
2016-07-01 00:32:11 +0200

    Keynote speakers update - Ranking

--- keynote.php
+++ keynote.php
@@ -12,26 +12,48 @@
 			  <h1>Keynote Speakers</h1>
 			</div>
 	    </div>
-		 <div class="row">
-		  <h2>Keynote Speakers from Sweden (KTH, Royal Institute of Technology)</h2>
-	    </div>
+		
 	    <div class="row">
-		  <div class="col-sm-6 col-md-4">
+		
+	       <div class="col-sm-6 col-md-4">
 		    <div class="thumbnail">
-		      <img src="img/speaker/1.jpg" class="img-circle" alt="Speaker1">
+		      <img src="img/speaker/13.jpg" class="img-circle" alt="Speaker13">
 		      <div class="caption">
-		        <h3><center>Prof/VP Ramon Wyss, KTH</center></h3>
-		        <p>Challenge driven education for energy system transformations</p>
+		        <h3><center>Dr.Mohamed Elyamani, MoEE</center></h3>
+		        <p>VP of the Ministry of Electrical and Renewable Energy<br />TBD</p>
 		      </div>
 		    </div>
 		  </div>
 
 		  <div class="col-sm-6 col-md-4">
 		    <div class="thumbnail">
-		      <img src="img/speaker/2.jpg" class="img-circle" alt="Speaker2">
+		      <img src="img/speaker/7.jpg" class="img-circle" alt="Speaker7">
 		      <div class="caption">
-		        <h3><center>Prof.Mark Howells, KTH</center></h3>
-		        <p>African Energy Renaissance – An immense investment, technology and sustainable development prospect</p>
+		        <h3><center>Prof.Björn Palm, KTH</center></h3>
+		        <p>Solar Cooling, status and perspectives</p>
+		      </div>
+		    </div>
+		  </div>
+
+		   <div class="col-sm-6 col-md-4">
+		    <div class="thumbnail">
+		      <img src="img/speaker/12-inserted.jpg" class="img-circle" alt="Speaker12ins">
+		      <div class="caption">
+		        <h3><center>Prof.Hossam Moghazy, MWRI</center></h3>
+		        <p>Chairman of Irrigation and Hydraulics, Previous Minister of Water Resources And Irrigation<br />Use of Solar Energy in the Project of Irrigation of 1.5 Million Acres in Egypt</p>
+		      </div>
+		    </div>
+		  </div>
+
+		</div>
+		<div class="row">
+		 
+		<div class="col-sm-6 col-md-4">
+		    <div class="thumbnail">
+		      <img src="img/speaker/17.jpg" class="img-circle" alt="Speaker17">
+		      <div class="caption">
+				<h3><center>Prof Taher Ahmed Al-Sahhaf, Kuwait University</center></h3>
+				<p>Vice president of research, Kuwait University<br />Sustainable Energy Research in Kuwait</p>
 		      </div>
 		    </div>
 		  </div>
@@ -45,15 +67,26 @@
 		      </div>
 		    </div>
 		  </div>
-		</div>
 
-		<div class="row">
 		  <div class="col-sm-6 col-md-4">
 		    <div class="thumbnail">
-		      <img src="img/speaker/4.jpg" class="img-circle" alt="Speaker4">
+		      <img src="img/speaker/15.jpg" class="img-circle" alt="Speaker15">
 		      <div class="caption">
-		        <h3><center>Prof.Mamoun Muhammed, KTH</center></h3>
-		        <p>TBD</p>
+		        <h3><center>Prof.Said ElNashaie, UBC</center></h3>
+		        <p>Adjunct Professor, Chemical and Biological Engineering Department, University of British Columbia, Vancouver, BC, Canada<br />Sustainable Development</p>
+		      </div>
+		    </div>
+		  </div>
+
+		</div>
+		<div class="row">
+
+		  <div class="col-sm-6 col-md-4">
+		    <div class="thumbnail">
+		      <img src="img/speaker/18.jpg" class="img-circle" alt="Speaker15">
+		      <div class="caption">
+		        <h3><center>Prof.Ibrahim Ismail, Zewail City</center></h3>
+		        <p>Director of Renewable Energy Engineering Program, Zewail City of Science and Technology<br />Potentialities of Solar Energy in Egypt</p>
 		      </div>
 		    </div>
 		  </div>
@@ -68,6 +101,19 @@
 		    </div>
 		  </div>
 
+		   <div class="col-sm-6 col-md-4">
+		    <div class="thumbnail">
+		      <img src="img/speaker/16.jpg" class="img-circle" alt="Speaker16">
+		      <div class="caption">
+				<h3><center>Dr Fahd Hashiesh, ABB</center></h3>
+				<p>FIET Head of Power Consulting, ABB Ltd, UK<br />The Grid Challenges &amp; Mitigations</p>
+		      </div>
+		    </div>
+		  </div>
+
+		</div>
+		<div class="row">
+
 		  <div class="col-sm-6 col-md-4">
 		    <div class="thumbnail">
 		      <img src="img/speaker/avatar.jpg" class="img-circle" alt="Speaker6">
@@ -77,15 +123,46 @@
 		      </div>
 		    </div>
 		  </div>
-		</div>
 
-		<div class="row">
-		  <div class="col-sm-6 col-md-4">
+		<div class="col-sm-6 col-md-4">
 		    <div class="thumbnail">
-		      <img src="img/speaker/7.jpg" class="img-circle" alt="Speaker7">
+		      <img src="img/speaker/2.jpg" class="img-circle" alt="Speaker2">
 		      <div class="caption">
-		        <h3><center>Prof.Björn Palm, KTH</center></h3>
-		        <p>Solar Cooling, status and perspectives</p>
+		        <h3><center>Prof.Mark Howells, KTH</center></h3>
+		        <p>African Energy Renaissance – An immense investment, technology and sustainable development prospect</p>
+		      </div>
+		    </div>
+		  </div>
+
+		   <div class="col-sm-6 col-md-4">
+		    <div class="thumbnail">
+		      <img src="img/speaker/10.jpg" class="img-circle" alt="Speaker10">
+		      <div class="caption">
+		        <h3><center>Prof.Yohannes Kiros, KTH</center></h3>
+		        <p>Alternative catalysts for anodic and cathodic reactions in water electrolysis</p>
+		      </div>
+		    </div>
+		  </div>
+
+		</div>
+		<div class="row">
+
+		   <div class="col-sm-6 col-md-4">
+		    <div class="thumbnail">
+		      <img src="img/speaker/9.jpg" class="img-circle" alt="Speaker9">
+		      <div class="caption">
+		        <h3><center>Prof.Alexis Pontvik, KTH</center></h3>
+		        <p>TBD.</p>
+		      </div>
+		    </div>
+		  </div>
+		
+		<div class="col-sm-6 col-md-4">
+		    <div class="thumbnail">
+		      <img src="img/speaker/11.jpg" class="img-circle" alt="Speaker11">
+		      <div class="caption">
+		        <h3><center>Prof.Björn Laumert, KTH</center></h3>
+		        <p>The role and future potential of CSP in the MENA region.</p>
 		      </div>
 		    </div>
 		  </div>
@@ -100,120 +177,6 @@
 		    </div>
 		  </div>
 
-		  <div class="col-sm-6 col-md-4">
-		    <div class="thumbnail">
-		      <img src="img/speaker/9.jpg" class="img-circle" alt="Speaker9">
-		      <div class="caption">
-		        <h3><center>Prof.Alexis Pontvik, KTH</center></h3>
-		        <p>TBD.</p>
-		      </div>
-		    </div>
-		  </div>
-		</div>
-		<div class="row">
-		  <div class="col-sm-6 col-md-4">
-		    <div class="thumbnail">
-		      <img src="img/speaker/10.jpg" class="img-circle" alt="Speaker10">
-		      <div class="caption">
-		        <h3><center>Prof.Yohannes Kiros, KTH</center></h3>
-		        <p>Alternative catalysts for anodic and cathodic reactions in water electrolysis</p>
-		      </div>
-		    </div>
-		  </div>
-
-		  <div class="col-sm-6 col-md-4">
-		    <div class="thumbnail">
-		      <img src="img/speaker/11.jpg" class="img-circle" alt="Speaker11">
-		      <div class="caption">
-		        <h3><center>Prof.Björn Laumert, KTH</center></h3>
-		        <p>The role and future potential of CSP in the MENA region.</p>
-		      </div>
-		    </div>
-		  </div>
-
-		  <div class="col-sm-6 col-md-4">
-		    <div class="thumbnail">
-		      <img src="img/speaker/12.jpg" class="img-circle" alt="Speaker12">
-		      <div class="caption">
-		        <h3><center>Prof.Cristian Bogdan, KTH</center></h3>
-		        <p>TBD</p>
-		      </div>
-		    </div>
-		  </div>
-		</div>  
-		 <div class="row">
-		  <h2>Keynote Speakers from Egypt and Kuwait</h2>
-		 </div>
-	    <div class="row">
-		  <div class="col-sm-6 col-md-4">
-		    <div class="thumbnail">
-		      <img src="img/speaker/12-inserted.jpg" class="img-circle" alt="Speaker12ins">
-		      <div class="caption">
-		        <h3><center>Prof.Hossam Moghazy, MWRI</center></h3>
-		        <p>Chairman of Irrigation and Hydraulics, Previous Minister of Water Resources And Irrigation<br />Use of Solar Energy in the Project of Irrigation of 1.5 Million Acres in Egypt</p>
-		      </div>
-		    </div>
-		  </div>
-		  <div class="col-sm-6 col-md-4">
-		    <div class="thumbnail">
-		      <img src="img/speaker/13.jpg" class="img-circle" alt="Speaker13">
-		      <div class="caption">
-		        <h3><center>Dr.Mohamed Elyamani, MoEE</center></h3>
-		        <p>VP of the Ministry of Electrical and Renewable Energy<br />TBD</p>
-		      </div>
-		    </div>
-		  </div>
-		  <div class="col-sm-6 col-md-4">
-		    <div class="thumbnail">
-		      <img src="img/speaker/14.jpg" class="img-circle" alt="Speaker14">
-		      <div class="caption">
-		        <h3><center>Dr. Abdel-Aty Edris, IEEE Fellow</center></h3>
-		        <p>Energy Storage and the Electric Grid of the Future (Smart Grids)</p>
-		      </div>
-		    </div>
-		  </div>
-		</div>
-	    <div class="row">
-		  <div class="col-sm-6 col-md-4">
-		    <div class="thumbnail">
-		      <img src="img/speaker/15.jpg" class="img-circle" alt="Speaker15">
-		      <div class="caption">
-		        <h3><center>Prof.Said ElNashaie, UBC</center></h3>
-		        <p>Adjunct Professor, Chemical and Biological Engineering Department, University of British Columbia, Vancouver, BC, Canada<br />Sustainable Development</p>
-		      </div>
-		    </div>
-		  </div>
-		  <div class="col-sm-6 col-md-4">
-		    <div class="thumbnail">
-		      <img src="img/speaker/16.jpg" class="img-circle" alt="Speaker16">
-		      <div class="caption">
-				<h3><center>Dr Fahd Hashiesh, ABB</center></h3>
-				<p>FIET Head of Power Consulting, ABB Ltd, UK<br />The Grid Challenges &amp; Mitigations</p>
-		      </div>
-		    </div>
-		  </div>
-		  <div class="col-sm-6 col-md-4">
-		    <div class="thumbnail">
-		      <img src="img/speaker/17.jpg" class="img-circle" alt="Speaker17">
-		      <div class="caption">
-				<h3><center>Prof Taher Ahmed Al-Sahhaf, Kuwait University</center></h3>
-				<p>Vice president of research, Kuwait University<br />Sustainable Energy Research in Kuwait</p>
-		      </div>
-		    </div>
-		  </div>
-	   </div>
-	   <div class="row">
-		  <div class="col-sm-6 col-md-4">
-		    <div class="thumbnail">
-		      <img src="img/speaker/18.jpg" class="img-circle" alt="Speaker15">
-		      <div class="caption">
-		        <h3><center>Prof.Ibrahim Ismail, Zewail City</center></h3>
-		        <p>Director of Renewable Energy Engineering Program, Zewail City of Science and Technology<br />Potentialities of Solar Energy in Egypt</p>
-		      </div>
-		    </div>
-		  </div>
-		</div>
-	  </div>
 	</div>
 
 <?php include('footer.php'); ?>
