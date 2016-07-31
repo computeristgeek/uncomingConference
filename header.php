87195cfe1ffde60d45437028f8895dd03bf362a3
Karim Soliman <karim.solimman@gmail.com>
2016-07-31 23:46:01 +0200

    Adding the carousel style for the news carousel in home.php

--- header.php
+++ header.php
@@ -19,6 +19,7 @@
     <link rel="stylesheet" type="text/css" href="css/navbar_blue.css">
     <link href="css/news.css" rel="stylesheet" type="text/css" />
     <link rel="stylesheet" type="text/css" href="css/newsfeed.css">
+    <link rel="stylesheet" type="text/css" href="css/carousel.css">
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
