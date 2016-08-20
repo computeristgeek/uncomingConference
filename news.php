719ea83ee5528aaed1856dd2ebc539cbf91d2c05
Karim Soliman <karim.solimman@gmail.com>
2016-08-20 15:34:41 +0200

    News page update

--- news.php
+++ news.php
@@ -1,31 +1,80 @@
 <?php
-	$title='ICNTSE - NEWS';
+	$title='ICNTSE - News';
 	$keywords='icntse,pua,kth,international conference,new trends,sustainable energy,science,news';
 	$description='ICNTSE RSS FEED NEWS';
 ?>
 <?php include 'header.php' ?>
-  <body style="padding-top: 50px;">
+  <body style="padding-top: 70px;">
 <?php include('navbar.php');?>
     <div class="container">
-        <div class="row">
-        	<div class="col-md-12">
-		<div>
-        		<div class="panel panel-default">
-						<div class="panel-heading"> <b>ICNTSE News Feed</b></div>
-							<div class="panel-body">
-								<div class="row">
-									<div class="col-xs-12">
-	<ul>
-<div> <!-- start feedwind code --><script type="text/javascript">document.write('\x3Cscript type="text/javascript" src="' + ('https:' == document.location.protocol ? 'https://' : 'http://') + 'feed.mikle.com/js/rssmikle.js">\x3C/script>');</script><script type="text/javascript">(function() {var params = {rssmikle_url: "http://www.icntse.com/rss.xml",rssmikle_frame_width: "100",rssmikle_frame_height: "400",frame_height_by_article: "0",rssmikle_target: "_blank",rssmikle_font: "Arial, Helvetica, sans-serif",rssmikle_font_size: "12",rssmikle_border: "off",responsive: "on",rssmikle_css_url: "",text_align: "left",text_align2: "left",corner: "on",scrollbar: "on",autoscroll: "off",scrolldirection: "down",scrollstep: "10",mcspeed: "20",sort: "Off",rssmikle_title: "off",rssmikle_title_sentence: "",rssmikle_title_link: "",rssmikle_title_bgcolor: "#FFFFFF",rssmikle_title_color: "#000000",rssmikle_title_bgimage: "",rssmikle_item_bgcolor: "#FFFFFF",rssmikle_item_bgimage: "",rssmikle_item_title_length: "100",rssmikle_item_title_color: "#000000",rssmikle_item_border_bottom: "on",rssmikle_item_description: "on",item_link: "off",rssmikle_item_description_length: "450",rssmikle_item_description_color: "#000000",rssmikle_item_date: "gl1",rssmikle_timezone: "Etc/GMT",datetime_format: "%b %e, %Y %l:%M %p",item_description_style: "text+tn",item_thumbnail: "full",item_thumbnail_selection: "auto",article_num: "15",rssmikle_item_podcast: "off",keyword_inc: "",keyword_exc: ""};feedwind_show_widget_iframe(params);})();</script><!-- end feedwind code --><!--  end  feedwind code --></div>
-	</ul>
-									</div>
-								</div>
-							</div>
-						<div class="panel-footer"><a href="rss.xml"><img id="rssimg" src="img/Rss-feed.svg" alt="Rss Feed"/></a></div>
+
+      <div class="row">
+				<div class="col-sm-6 col-md-4">
+			    <div class="thumbnail">
+			      <img src="img/journal.png">
+			      <div class="caption">
+			        <h3>Renewable Energy journal</h3>
+			        <p>Selected papers will be submitted to the journal of "Renewable energy and sustainable development" for possible publications. Authors had to follow up this with the journal editor.
+							</p>
+			      </div>
+			    </div>
+			  </div>
+
+				<div class="col-sm-6 col-md-4">
+					<div class="thumbnail">
+						<img src="img/fees.png">
+						<div class="caption">
+							<h3>Late Fees</h3>
+							<p>Late full paper submission fees will be 900 LE, By September 15, 2016</p>
+						</div>
+					</div>
 				</div>
-			</div>
-        	</div>
+
+				<div class="col-sm-6 col-md-4">
+					<div class="thumbnail">
+						<img src="img/deadline.png">
+						<div class="caption">
+							<h3>Submission Deadlines</h3>
+							<p>Final paper submission to the conference has been closed by August 15, 2016</p>
+						</div>
+					</div>
+				</div>
+
     	</div>
+
+			<div class="row">
+				<div class="col-sm-6 col-md-4">
+			    <div class="thumbnail">
+			      <img src="img/fees.png">
+			      <div class="caption">
+			        <h3>Late Fees</h3>
+			        <p>Late registration fees for attendance is 500 LE, After August 1st, 2016</p>
+			      </div>
+			    </div>
+			  </div>
+
+				<div class="col-sm-6 col-md-4">
+					<div class="thumbnail">
+						<img src="img/deadline.png">
+						<div class="caption">
+							<h3>Submission Deadlines</h3>
+							<p>Keynote speakers submission to the conference has been closed by August 15, 2016</p>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-sm-6 col-md-4">
+					<div class="thumbnail">
+						<img src="img/serial.png">
+						<div class="caption">
+							<h3>Conference ISSN</h3>
+							<p>Conference International Standard Serial Numbers<br/>ISSN: 2357-0865 (print), 2357-0873 (online)</p>
+						</div>
+					</div>
+				</div>
+
+    	</div>
+
     </div>
 	<script>$('#newsfeed').toggle()</script>
 <?php include 'footer.php' ?>
