3cbd869870c7c5ff5bc6f543e7d96258f2b38f7a
Karim Soliman <karim.solimman@gmail.com>
2016-09-09 16:03:46 +0200

    Adding footer to the website

--- footer.php
+++ footer.php
@@ -1,2 +1,8 @@
+  <footer class="footer">
+      <div class="container_footer">
+        <p class="text-muted">All Rights recived. PUA - Pharos University in Alexandria</p>
+      </div>
+    </footer>
+
   </body>
 </html>
