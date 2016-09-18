5da30e471cb464db0f0319e218bc778944c3d6ac
Karim Soliman <karim.solimman@gmail.com>
2016-09-19 01:07:34 +0200

    New web page called documents includes the conference documents

---
+++ documents.php
@@ -0,0 +1,38 @@
+<?php
+	$keywords='icntse,pua,kth,international conference,new trends,sustainable energy,science,extended abstract,keynote,speakers';
+	$description='This page lists the ICNTSE Conference Keynotes.';
+	$title='Documents - ICNTSE';
+?>
+<?php include('header.php'); ?>
+<body style="padding-top:50px;">
+	<?php include('navbar.php'); ?>
+	<div class="container">
+		 <div class="row">
+			<div class="page-header">
+			  <h1>Conference Documents</h1>
+			</div>
+	    </div>
+
+	    <div class="row">
+	    	<div class="col-md-6">
+	    		<img src="img/book.png" class="img-responsive" style="margin:0 auto;">
+	    		<h2 class="text-center">Book of Abstracts</h2>
+	    		<div class="text-center">
+	    			<a class="btn btn-primary" href="abstract_book.pdf">Click here</a>
+	    		</div>
+	    	</div>
+
+	    	<div class="col-md-6">
+	    		<div class="col-md-6">
+	    		<img src="img/paper.png" class="img-responsive" style="margin:0 auto;">
+	    		<h2 class="text-center">List of Papers</h2>
+	    		<div class="text-center">
+	    			<a class="btn btn-primary" href="PapersList_Day01.pdf">Day 1</a>
+	    			<a class="btn btn-primary" href="PapersList_Day02.pdf">Day 2</a>
+	    			<a class="btn btn-primary" href="PapersList_Day03.pdf">Day 3</a>
+	    		</div>
+	    	</div>
+	    </div>
+	</div>
+</div>
+<?php include('footer.php'); ?>
