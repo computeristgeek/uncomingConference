05d567adea1aa231d6e9987b70baf0a6f20fc3a5
Karim Soliman <karim.solimman@gmail.com>
2016-10-04 19:15:34 +0200

    Keynotes speeches

---
+++ keynote_speeches.php
@@ -0,0 +1,108 @@
+<?php
+	$keywords='icntse,pua,kth,international conference,new trends,sustainable energy,science,extended abstract,keynote,speakers';
+	$description='This page lists the ICNTSE Conference Keynotes.';
+	$title='Keynote Speeches - ICNTSE';
+?>
+<?php include('header.php'); ?>
+<body style="padding-top:50px;">
+	<?php include('navbar.php'); ?>
+	<div class="container">
+		 <div class="row">
+			<div class="page-header">
+			  <h1>Keynote Speeches</h1>
+			</div>
+	    </div>
+
+	<div class="row">
+		<div class='col-md-12'>
+		<table class="table table-hover">
+			<tr>
+				<th>#</th>
+				<th>Keynote</th>
+				<th>Title</th>
+				<th>Download</th>
+			</tr>
+			<tr>
+				<td>1</td>
+				<td>Mohamed Elyamani</td>
+				<td>الاتجاهات الحديثة للطاقة الجديدة والمتجددة بجمهورية مصر العربية</td>
+				<td><a href="keynote_speeches/Yammani.pdf">Download</a></td>
+			</tr>
+			<tr>
+				<td>2</td>
+				<td>Said Salah Eldin Elnashaie</td>
+				<td>Sustainable Development Engineering</td>
+				<td><a href="keynote_speeches/Elnashaie.pdf">Download</a></td>
+			</tr>
+			<tr>
+				<td>3</td>
+				<td>G. Engdahl</td>
+				<td>Electric Power Component Design</td>
+				<td><a href="keynote_speeches/Engdahl.pdf">Download</a></td>
+			</tr>
+			<tr>
+				<td>4</td>
+				<td>Björn Laumert</td>
+				<td>Solar Power CSP</td>
+				<td><a href="keynote_speeches/Laumert.pdf">Download</a></td>
+			</tr>
+			<tr>
+				<td>5</td>
+				<td>Hossam Moghazy</td>
+				<td>Renewable Energy For Sustainable Groundwater Use in 1.5 Million Feddans Mega Project</td>
+				<td><a href="keynote_speeches/Mogazi.pdf">Download</a></td>
+			</tr>
+			<tr>
+				<td>6</td>
+				<td>Taher Al-Sahhaf</td>
+				<td>Sustainable Energy Research and Development in Kuwait</td>
+				<td><a href="keynote_speeches/SER_Kuwait.pdf">Download</a></td>
+			</tr>
+			<tr>
+				<td>7</td>
+				<td>Hesham El Shimy</td>
+				<td>Green House as a Concept Design</td>
+				<td><a href="keynote_speeches/Hesham.pdf">Download</a></td>
+			</tr>
+			<tr>
+				<td>8</td>
+				<td>Klas Engvall</td>
+				<td>Trends in Thermochemical conversion of waste and biomass</td>
+				<td><a href="keynote_speeches/Klas.pdf">Download</a></td>
+			</tr>
+			<tr>
+				<td>9</td>
+				<td>Caroline van der Weerdt, Richard Carmichael, Björn Palm</td>
+				<td>ICT for Community Energy</td>
+				<td><a href="keynote_speeches/CRB.pdf">Download</a></td>
+			</tr>
+			<tr>
+				<td>10</td>
+				<td>Björn Palm</td>
+				<td>Solar cooling – Status and perspectives</td>
+				<td><a href="keynote_speeches/Palm.pdf">Download</a></td>
+			</tr>
+			<tr>
+				<td>11</td>
+				<td>Radwan H. Abdel Hamid</td>
+				<td>Multipurpose Applications by Thermodynamic Solar</td>
+				<td><a href="keynote_speeches/Radwan.pdf">Download</a></td>
+			</tr>
+			<tr>
+				<td>12</td>
+				<td>Björn Palm</td>
+				<td>Future Energy Systems</td>
+				<td><a href="keynote_speeches/Palm_2.pdf">Download</a></td>
+			</tr>
+			<tr>
+				<td>13</td>
+				<td>Alexandria Fertilizers Company</td>
+				<td>Energy Conservation Efforts in Ammonia-urea Fertilizer Plants</td>
+				<td><a href="keynote_speeches/AFC.pdf">Download</a></td>
+			</tr>
+		</table>
+		</div>
+	</div>
+
+</div>
+<?php include('footer.php'); ?>
