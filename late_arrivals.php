59b4301e7c9de3ab5325a85e71c3148359bdf7ce
Karim Soliman <karim.solimman@gmail.com>
2016-10-17 23:18:31 +0200

    Late arrivals page

---
+++ late_arrivals.php
@@ -0,0 +1,53 @@
+<?php
+	$keywords='icntse,pua,kth,international conference,new trends,sustainable energy,science,extended abstract,keynote,speakers';
+	$description='This page lists the ICNTSE Conference Late arrivals speeches.';
+	$title='Keynote Speeches - ICNTSE';
+?>
+<?php include('header.php'); ?>
+<style type="text/css">
+	td{
+		font-weight: bold;
+	}
+</style>
+<body style="padding-top:50px;">
+	<?php include('navbar.php'); ?>
+	<div class="container">
+		 <div class="row">
+			<div class="page-header">
+			  <h1>Keynote Speeches</h1>
+			</div>
+	    </div>
+
+	<div class="row">
+		<div class='col-md-12'>
+		<table class="table table-hover">
+			<tr>
+				<th>#</th>
+				<th>Keynote</th>
+				<th>Title</th>
+				<th>Download</th>
+			</tr>
+			<tr>
+				<td>1</td>
+				<td>Björn Palm</td>
+				<td>Solar cooling – Status and perspectives</td>
+				<td><a href="keynote_speeches/Palm.pdf">Download</a></td>
+			</tr>
+			<tr>
+				<td>2</td>
+				<td>Caroline van der Weerdt, Richard Carmichael, Björn Palm</td>
+				<td>ICT for Community Energy</td>
+				<td><a href="keynote_speeches/CRB.pdf">Download</a></td>
+			</tr>
+			<tr>
+				<td>3</td>
+				<td>Alexandria Fertilizers Company</td>
+				<td>Energy Conservation Efforts in Ammonia-Urea Fertilizer Plants</td>
+				<td><a href="keynote_speeches/AFC.pdf">Download</a></td>
+			</tr>
+		</table>
+		</div>
+	</div>
+
+</div>
+<?php include('footer.php'); ?>
